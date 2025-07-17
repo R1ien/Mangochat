84b058bf6467b56fd1add8b6c792c5c1f2be77da
R1ien <163441483+R1ien@users.noreply.github.com>
2025-07-17 20:14:49 +0200

    Update call.js

--- public/call.js
+++ public/call.js
@@ -1,1 +1,10 @@
+console.log("📞 Module d’appel chargé !");
 
+async function startMicro() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    console.log("✅ Micro activé !");
+  } catch (err) {
+    console.error("❌ Erreur micro :", err);
+  }
+}
